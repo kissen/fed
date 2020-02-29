0f885c2505f27f86ef6fa30201259d780d9a8f20
Andreas Schärtl <andreas.schaertl@fau.de>
2020-02-29 11:16:45 +0100

    FedTransport: use HTTP timeouts

--- ap/fed_transport.go
+++ ap/fed_transport.go
@@ -10,13 +10,27 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
+
+// The content type to use for PUT (Content Type) and GET (Accept)
+const _CONTENT_TYPE = `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`
+
+// Timeout for our HTTP requests. I guess eventually we will implement
+// retries and the like; at that point the timeouts will become more complex
+const _HTTP_TIMEOUT = 8 * time.Second
 
 // Implements the go-fed/activity/pub/FedTransport interface (version 1.0)
 type FedTransport struct {
 	Context   context.Context
 	Target    *url.URL // TODO: use for authentication
 	UserAgent string   // TODO: use in http requests
+
+	// http client used for http requests; use client() to get a usable
+	// client
+	cachedClient     *http.Client
+	cachedClientLock sync.Mutex
 }
 
 // Dereference fetches the ActivityStreams object located at this IRI
@@ -34,19 +48,30 @@ func (f *FedTransport) Dereference(c context.Context, iri *url.URL) ([]byte, err
 }
 
 // Deliver sends an ActivityStreams object.
-func (f *FedTransport) Deliver(c context.Context, b []byte, to *url.URL) error {
+func (f *FedTransport) Deliver(c context.Context, b []byte, to *url.URL) (err error) {
 	log.Printf("Deliver(%v)", to)
 
-	// create a copy of the raw bytes as they will get consumed by us
+	// preapre the io.Reader that contains the request body
 
 	copy := f.copyOf(b)
+	body := bytes.NewReader(copy)
+
+	// build up the request
+
+	var req *http.Request
+
+	if req, err = http.NewRequest("PUT", to.String(), body); err != nil {
+		return errors.Wrap(err, "cannot set up request")
+	}
+
+	req.Header.Set("Content-Type", _CONTENT_TYPE)
 
 	// POST to the address
 
-	resp, err := http.Post(to.String(), "application/ld+json", bytes.NewBuffer(copy))
+	var resp *http.Response
 
-	if err != nil {
-		return err
+	if resp, err = f.client().Do(req); err != nil {
+		return errors.Wrap(err, "cannot do request")
 	}
 
 	defer resp.Body.Close()
@@ -54,7 +79,7 @@ func (f *FedTransport) Deliver(c context.Context, b []byte, to *url.URL) error {
 	// evaluate result
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("server to=%v rejected with status=%v", to, resp.Status)
+		return fmt.Errorf("to=%v replied status=%v", to, resp.Status)
 	}
 
 	return nil
@@ -64,7 +89,7 @@ func (f *FedTransport) Deliver(c context.Context, b []byte, to *url.URL) error {
 func (f *FedTransport) BatchDeliver(c context.Context, b []byte, recipients []*url.URL) error {
 	log.Printf("BatchDeliver(%v)", recipients)
 
-	// XXX: slow and wrong (quits halfway through)
+	// XXX: slow and wrong (quits halfway through on errors)
 
 	for _, recipent := range recipients {
 		if err := f.Deliver(c, b, recipent); err != nil {
@@ -99,7 +124,7 @@ func (f *FedTransport) dereferenceFromRemote(c context.Context, iri *url.URL) ([
 	// evaluate result
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server at iri=%v rejected with status=%v", iri, resp.Status)
+		return nil, fmt.Errorf("iri=%v replied status=%v", iri, resp.Status)
 	}
 
 	// read into buffer
@@ -112,8 +137,27 @@ func (f *FedTransport) dereferenceFromRemote(c context.Context, iri *url.URL) ([
 	return buf.Bytes(), nil
 }
 
+// Return a copy of src.
 func (f *FedTransport) copyOf(src []byte) []byte {
 	dst := make([]byte, len(src))
 	copy(dst, src)
 	return dst
+}
+
+// Return an HTTP client usable for requests. It comes with a timeout so we don't
+// get stuck waiting forever.
+func (f *FedTransport) client() *http.Client {
+	f.cachedClientLock.Lock()
+	defer f.cachedClientLock.Unlock()
+
+	// only create one client per transport and re-use them; see
+	// https://golang.org/pkg/net/http/#Client for details
+
+	if f.cachedClient == nil {
+		f.cachedClient = &http.Client{
+			Timeout: _HTTP_TIMEOUT,
+		}
+	}
+
+	return f.cachedClient
 }
