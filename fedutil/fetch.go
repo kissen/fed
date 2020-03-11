6681f2dfeeef02d72a8f87987aea649fe292b090
Andreas Schärtl <andreas.schaertl@fau.de>
2020-03-11 10:53:01 +0100

    Switch from custom VocabCache to complcache
    We'll need to cache more anyways

--- fedutil/fetch.go
+++ fedutil/fetch.go
@@ -2,18 +2,43 @@ package fedutil
 
 import (
 	"github.com/go-fed/activity/streams/vocab"
+	"github.com/kissen/complcache"
 	"github.com/pkg/errors"
 	"net/url"
+	"log"
+	"time"
 )
 
-var fetchCache VocabCache
+const _EXPIRATION = 30 * time.Second
+const _FILL = 15 * time.Second
+const _GC = 4 * time.Minute
+
+var fetchCache complcache.Cache
 
 func init() {
-	fetchCache = NewVocabCache()
+	var err error
+
+	if fetchCache,err = complcache.New(_EXPIRATION, _FILL, _GC); err != nil {
+		log.Panic("cannot create http cache:", err)
+	}
 }
 
 func Fetch(iri *url.URL) (vocab.Type, error) {
-	return fetchCache.Get(iri)
+	creator := func() (interface{}, error) {
+		if raw, err := Get(iri); err != nil {
+			return nil, err
+		} else if obj, err := BytesToVocab(raw); err != nil {
+			return nil, err
+		} else {
+			return obj, nil
+		}
+	}
+
+	if obj, err := fetchCache.GetOrCreate(iri.String(), creator); err != nil {
+		return nil, err
+	} else {
+		return obj.(vocab.Type), nil
+	}
 }
 
 func FetchString(addr string) (vocab.Type, error) {
