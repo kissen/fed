9df7ec44554ec54ae3dccc4c5200ad1c98ad024a
Andreas Schärtl <andreas@schaertl.me>
2020-03-23 12:59:32 +0100

    fedd/main: split up into more funcs

--- fedd/main.go
+++ fedd/main.go
@@ -9,97 +9,80 @@ import (
 	"net/http"
 )
 
-func openDatabase() db.FedStorage {
+func OpenDatabase() db.FedStorage {
 	// open db
-
-	storage := &db.FedEmbeddedStorage{
-		Filepath: Config().Store,
-	}
-
+	storage := &db.FedEmbeddedStorage{Filepath: Config().Store}
 	if err := storage.Open(); err != nil {
 		log.Fatal(err)
 	}
 
-	// add some users
-
-	usernames := []string{
-		"alice", "bob", "celia", "daniel", "emily", "frank",
-	}
-
-	for _, username := range usernames {
+	// add some users for testing
+	for _, username := range []string{"alice", "bob", "celia"} {
 		user := &db.FedUser{Name: username}
 		Must(storage.StoreUser(user))
 	}
 
-	// done
-
 	return storage
 }
 
-func listenAndAccept(storage db.FedStorage) {
-	// set up ap/ structs
-
+func InstallApHandlers(storage db.FedStorage, router *mux.Router) {
+	// set up required structs that implement the various go-fed interfaces
 	common := &ap.FedCommonBehavior{}
 	socialProtocol := &ap.FedSocialProtocol{}
 	fedProtocol := &ap.FedFederatingProtocol{}
 	database := &ap.FedDatabase{}
 	clock := &ap.FedClock{}
 
-	actor := pub.NewActor(
-		common, socialProtocol, fedProtocol, database, clock,
-	)
+	// set up go-fed proxies
+	actor := pub.NewActor(common, socialProtocol, fedProtocol, database, clock)
+	handler := pub.NewActivityStreamsHandler(database, clock)
 
-	handler := pub.NewActivityStreamsHandler(
-		database, clock,
-	)
-
-	admin := &ap.FedAdminProtocol{}
-
-	// build up http handlers
-
+	// create http handler closures
 	inboxHandler := newInboxHandler(actor, storage)
 	outboxHandler := newOutboxHandler(actor, storage)
 	activityHandler := newActivityHandler(handler, storage)
+
+	// in/outbox handlers
+	router.HandleFunc(`/{username:[A-Za-z]+}/inbox`, inboxHandler).Methods("GET", "POST")
+	router.HandleFunc(`/{username:[A-Za-z]+}/outbox`, outboxHandler).Methods("GET", "POST")
+
+	// everything else
+	// TODO: Think about using just a catchall here? Gets in the way of other routes
+	// though (admin, oauth &c).
+	activityRoutes := []string{
+		`/storage/{id:[A-Za-z0-9\-]+}`,
+		`/{username:[A-Za-z]+}/followers`,
+		`/{username:[A-Za-z]+}/following`,
+		`/{username:[A-Za-z]+}/liked`,
+		`/{username:[A-Za-z]+}`,
+	}
+	for _, route := range activityRoutes {
+		router.HandleFunc(route, activityHandler).Methods("GET", "POST")
+	}
+
+	// middleware that installes the FedContext on all requests
+	router.Use(InstallBaseContext(storage))
+}
+
+func InstallAdminHandlers(storage db.FedStorage, router *mux.Router) {
+	admin := &ap.FedAdminProtocol{}
 	adminHandler := newAdminHandler(admin, storage)
 
-	// configure routes
-
-	router := mux.NewRouter().StrictSlash(false)
-	sr := router.PathPrefix(Config().Base.Path).Subrouter()
-
-	sr.HandleFunc(`/{username:[A-Za-z]+}/inbox`, inboxHandler).Methods("GET", "POST")
-	sr.HandleFunc(`/{username:[A-Za-z]+}/outbox`, outboxHandler).Methods("GET", "POST")
-
-	activityRoutes := []string{
-		`/{username:[A-Za-z]+}`, `/{username:[A-Za-z]+}/followers`,
-		`/{username:[A-Za-z]+}/following`, `/{username:[A-Za-z]+}/liked`,
-		`/storage/{id:[A-Za-z0-9\-]+}`,
-	}
-
-	for _, route := range activityRoutes {
-		sr.HandleFunc(route, activityHandler).Methods("GET", "POST")
-	}
-
-	// build up admin routes
-
-	sr.HandleFunc(`/{username:[A-Za-z]+}`, adminHandler).Methods("PUT")
-
-	// install midleware
-
-	sr.Use(InstallBaseContext(storage))
-
-	// let's rock!
-
-	addr := Config().Base.Host
-	log.Printf("starting on addr=%v...", addr)
-	Must(http.ListenAndServe(addr, router))
+	router.HandleFunc(`/{username:[A-Za-z]+}`, adminHandler).Methods("PUT")
 }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	storage := openDatabase()
+	storage := OpenDatabase()
 	defer storage.Close()
 
-	listenAndAccept(storage)
+	router := mux.NewRouter().StrictSlash(false)
+	sr := router.PathPrefix(Config().Base.Path).Subrouter()
+	InstallApHandlers(storage, sr)
+	InstallAdminHandlers(storage, sr)
+
+	addr := Config().Base.Host
+	log.Printf("starting on addr=%v...", addr)
+	Must(http.ListenAndServe(addr, router))
 }
