3c1032701b61bb88534db9a7cdb272c2c80eed5d
Andreas Schärtl <andreas@schaertl.me>
2020-04-01 12:41:52 +0200

    fetch: add IRI and IRIs method
    They convert an iterator to a slice of URLs.

---
+++ fetch/iri.go
@@ -0,0 +1,46 @@
+package fetch
+
+import (
+	"errors"
+	"gitlab.cs.fau.de/kissen/fed/prop"
+	"net/url"
+)
+
+// Given ie, return the IRI to that entry.
+//
+// This will not involve any network request, the
+// information is sourced from ie alone.
+func IRI(ie IterEntry) (iri *url.URL, err error) {
+	if !ie.HasAny() {
+		return nil, errors.New("ie is empty")
+	}
+
+	// try out iri
+
+	if ie.IsIRI() {
+		return ie.GetIRI(), nil
+	}
+
+	// try out object
+
+	obj := ie.GetType()
+	return prop.Id(obj), nil
+}
+
+// Iterate over it and return IRIs to each element
+// in that iterator.
+//
+// This will not involve any network request, the
+// information is sourced from ie alone.
+func IRIs(it Iter) (iris []*url.URL, err error) {
+	for ; it != it.End(); it = it.Next() {
+		iri, err := IRI(it)
+		if err != nil {
+			return nil, err
+		}
+
+		iris = append(iris, iri)
+	}
+
+	return iris, nil
+}
