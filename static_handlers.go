36fc845189fa7b46b2452a31bf1a884e41b7fee0
Andreas Schärtl <andreas@schaertl.me>
2020-04-14 14:10:26 +0200

    Staticly compile in /static resources

--- static_handlers.go
+++ static_handlers.go
@@ -1,40 +1,29 @@
 package main
 
 import (
-	"gitlab.cs.fau.de/kissen/fed/template"
-	"io/ioutil"
+	"github.com/gobuffalo/packr"
 	"log"
 	"mime"
 	"net/http"
 	"path"
-	"path/filepath"
 )
 
 // GET /static/*
 func GetStatic(w http.ResponseWriter, r *http.Request) {
 	log.Printf("GetStatic(%v)", r.URL)
 
-	// find requested file on file system and read it into a buffer
+	box := packr.NewBox("static")
+	filename := path.Base(r.URL.Path)
 
-	name := path.Base(r.URL.Path)
-	relpath := filepath.Join("res", name)
-
-	content, err := ioutil.ReadFile(relpath)
-	if err != nil {
-		template.Error(w, r, http.StatusNotFound, err, nil)
+	if !box.Has(filename) {
+		DoError(w, r, "no resource with that name", http.StatusNotFound)
 		return
 	}
 
-	// set the correct mimetype header
-
-	mimetype := mime.TypeByExtension(path.Ext(name))
+	mimetype := mime.TypeByExtension(path.Ext(filename))
 	w.Header().Add("Content-Type", mimetype)
 
-	// write out the contents
-
-	w.WriteHeader(http.StatusOK)
-
-	if _, err := w.Write(content); err != nil {
-		log.Printf("writing static file to client failed: %v", err)
+	if _, err := w.Write(box.Bytes(filename)); err != nil {
+		log.Printf(`serving static filename="%v" failed with err="%v"`, filename, err)
 	}
 }
